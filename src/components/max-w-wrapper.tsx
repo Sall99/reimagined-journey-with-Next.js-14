8454a0ed8478b1888a2c683cc4f2c5dfcc7c6751
Sall <sallaboudaouda@gmail.com>
2024-03-02 16:36:21 +0000

    first commit

---
+++ src/components/max-w-wrapper.tsx
@@ -0,0 +1,20 @@
+import { cn } from "@/lib/utils";
+import React, { FC, ReactNode } from "react";
+
+interface MaxWidthWrapperProps {
+  className?: string;
+  children: ReactNode;
+}
+
+export default function MaxWidthWrapper({
+  className,
+  children,
+}: MaxWidthWrapperProps) {
+  return (
+    <main
+      className={cn("mx-auto w-full max-h-screen px-2 md:px-20", className)}
+    >
+      {children}
+    </main>
+  );
+}
